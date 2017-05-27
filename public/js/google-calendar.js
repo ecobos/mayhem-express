35e879f3a2b7c38c7cac311980d303959d336a27
Edgar Cobos <ecobos@edcobos.com>
2017-05-27 18:22:29 -0500

    almost done

--- public/js/google-calendar.js
+++ public/js/google-calendar.js
@@ -11,11 +11,46 @@ var settings = {
 
 $.ajax(settings).done(function (response) {
   console.log(response);
-  var nextEvent = response.items[0];
-  $('#cal-next-event').append('<div class="row"><div class="col-md-4" id="nextEventDate"><p id="nextEventMonth">'+ nextEvent.start.dateMonth +'</p><p id="nextEventDayNumber">'+ nextEvent.start.dateDayNumber +'</p></div><div class="col-md-8"><h1>' + nextEvent.summary + '</h1></div></div><h4>' + nextEvent.description + '</h4><div id="nextEventWhere"><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span><span>' + nextEvent.location + '</span></div><div id="nextEventWhen"><span class="glyphicon glyphicon-time" aria-hidden="true"></span><span>' + nextEvent.start.time + ' - ' + nextEvent.end.time + '</span></div>');
+  let nextEvent = response.events[0];
+  $('#cal-next-event').append(`
+    <div class="row">
+      <div class="col-md-4" id="nextEventDate">
+        <p id="nextEventMonth">${nextEvent.start.dateMonth}</p>
+        <p id="nextEventDayNumber">${nextEvent.start.dateDayNumber}</p>
+      </div>
+      <div class="col-md-8">
+        <h1>${nextEvent.summary}</h1>
+      </div>
+    </div>
+    <h4>${nextEvent.description}</h4>
+    <div id="nextEventWhere">
+      <span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
+      <span>${nextEvent.location}</span>
+    </div>
+    <div id="nextEventWhen">
+      <span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+      <span>${nextEvent.start.time} - ${nextEvent.end.time}</span>
+    </div>
+  `);
 
-  for(i = 1; i < response.items.length; i++){
-    $('tbody').append('<tr><td class="event-date">'+ response.items[i].start.dateFormatShort + ' @ ' + response.items[i].start.time +'</td><td class="event-summary">'+ response.items[i].summary +'</td></tr>');
-  }
   
+  for (i = 1; i < response.events.length; i++) {
+    $('tbody').append(`
+    <tr data-link="${response.events[i].htmlLink}">
+      <td class="event-date">${response.events[i].start.dateFormatShort} @ ${response.events[i].start.time}</td>
+      <td class="event-summary">${response.events[i].summary}</td>
+    </tr>
+    `);
+  }
+
+  $("tr").click(function(){
+    console.log($(this).attr("data-link"));
+    var url = $(this).attr("data-link"); 
+    window.open(url, '_blank');
+  });
+
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  })
+
 });
